4e4dc64eb5835e21339dee521ee5432ecb6334cf
kriomanser2000 <160469777+kriomanser2000@users.noreply.github.com>
2024-07-20 17:41:01 +0300

    Add files via upload

---
+++ HelloWorldEACHOTHER.php
@@ -0,0 +1,6 @@
+<?php
+echo "Hello\nWorld!";
+echo "Hello\n\tWorld!";
+echo "Hello World. \\\"My supa dupa page!\\\"";
+echo "<h1>Hello</h1> <h3>World</h3> <b>and</b> <i>alo</i> <u>sasa cool</u>";
+echo "<script> alert('Hello World'); </script>";
